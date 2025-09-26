75da1a8c6bbf491d500ee64fbfe57635f65398ef
PragalvaXFREZ <pragalva.sapkota@deerwalk.edu.np>
2025-09-26 17:39:15 +0545

    learnt loop and code simplification

--- analyse-log.sh
+++ analyse-log.sh
@@ -1,35 +1,29 @@
 #!/bin/bash
 
-# Use absolute path if possible
-LOG_DIR="."  
-APP_LOG_FILE="application.log"
-SYS_LOG_FILE="system.log"
+LOG_DIR="."
+ERROR_PATTERN=("ERROR" "FATAL" "CRITICAL")
 
 echo "analysing log files"
 echo "=================="
 
-echo -e "\n List of log files updated in last 24 hours"
-# Example: find /Users/John/logs -name "*.log" -mtime -1
-find "$LOG_DIR" -name "*.log" -mtime -1
+echo -e "\nList of log files updated in last 24 hours"
+LOG_FILES=$(find "$LOG_DIR" -name "*.log" -mtime -1)
+echo "$LOG_FILES"
 
-echo -e "\n searching for ERROR logs in application.log file"
-grep "ERROR" "$LOG_DIR/$APP_LOG_FILE"
+# iterate over each found file
+for LOG_FILE in $LOG_FILES; do
 
-echo -e "\n Number of ERROR logs found in application.log"
-grep -c "ERROR" "$LOG_DIR/$APP_LOG_FILE"
+echo -e "\n"
+echo "-------------------------------"
+echo "Analyzing file: $LOG_FILE"
+echo "-------------------------------"
 
-echo -e "\n searching for FATAL logs found in application.log"
-grep "FATAL" "$LOG_DIR/$APP_LOG_FILE"
+for PATTERN in "${ERROR_PATTERN[@]}"; do
 
-echo -e "\n Number of FATAL logs found in application.log"
-grep -c "FATAL" "$LOG_DIR/$APP_LOG_FILE"
+    echo -e "\n--- Processing: $LOG_FILE ---"
 
-echo -e "\n No of FATAL logs in system.log"
-grep -c "FATAL" "$LOG_DIR/$SYS_LOG_FILE"
+    echo -e "\nSearching for $PATTERN logs in $LOG_FILE"
+    grep "$PATTERN" "$LOG_FILE"
 
-echo -e "\n No of CRITICAL logs in system.log"
-grep -c "CRITICAL" "$LOG_DIR/$SYS_LOG_FILE"
-
-echo -e "\n Searching for CRITICAL logs in system.log"
-grep "CRITICAL" "$LOG_DIR/$SYS_LOG_FILE"
-
+    done
+done
